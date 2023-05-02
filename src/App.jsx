64841552b1f0007dd10c3ae3e7514d6248c0df32
XimenaLargo96 <ximena0301@gmail.com>
2023-05-01 23:52:35 -0500

    agrego redireccion al home

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-
-import { useContext } from "react";
+// eslint-disable-next-line
+import { useContext , useEffect } from "react";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet , useNavigate , useLocation} from "react-router-dom";
 import { ContextGlobal } from "./Components/utils/global.context";
 
 function App() {
 
+  const location = useLocation();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		location.pathname === "/" && navigate("/home");
+	}, []);
   const {state} = useContext(ContextGlobal)
 
   return (
       <div className={state.theme ? 'dark':'light'}>
           <Navbar/>
+          <main>
           <Outlet />
+          </main>
           <Footer/>
       </div>
   );
